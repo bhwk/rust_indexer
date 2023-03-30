07bd1c6928d4b7041f9bf519ae8ea92c6f38cbec
bhwk <120708160+bhwk@users.noreply.github.com>
2023-03-30 22:56:50 +0800

    feat: render div according to search result, open file

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,26 @@
 <script lang="ts">
 	import { invoke } from '@tauri-apps/api';
 
-	let dirPath = '/root/sideprojects/rust_indexer/';
-	let term: String;
+	let dirPath = '/root';
+	let term: string;
 	let searchResults: any;
+
+	function isBlank(str: string) {
+		return !str || /^\s*$/.test(str);
+	}
 
 	async function build_index() {
 		await invoke('build_index', { dirPath }).then(() => console.log('Indexing complete'));
 	}
 
-	async function search_files(term: String) {
+	async function open_file(path: string) {
+		await invoke('open_file', { path: path })
+			.then(() => console.log('File opened successfully'))
+			.catch((err) => console.log(err));
+	}
+
+	async function search_files(term: string) {
+		if (isBlank(term)) return;
 		let results;
 		await invoke('search_files', { term: term })
 			.then((data) => {
@@ -30,6 +41,13 @@
 <input bind:value={term} />
 {#if searchResults}
 	{#each Object.entries(searchResults) as [path, filename]}
-		<div>{filename}: {path}</div>
+		<div>
+			<div>{filename}: {path}</div>
+			<button
+				on:click={() => {
+					open_file(path);
+				}}>Open file</button
+			>
+		</div>
 	{/each}
 {/if}
